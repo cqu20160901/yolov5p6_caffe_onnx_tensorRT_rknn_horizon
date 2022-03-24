aa2ea111eb90aa4649da3926bafd40a5f3ca1209
业余程序猿 <1013429609@qq.com>
2022-03-24 10:32:35 +0800

    Update yolov5_demo_caffe.py

--- caffe_yolov5p6/yolov5_demo_caffe.py
+++ caffe_yolov5p6/yolov5_demo_caffe.py
@@ -143,9 +143,13 @@ def postprocess(out, img_h, img_w):
                             xmax = (bx + bw / 2) * scale_w
                             ymax = (by + bh / 2) * scale_h
 
-                            if xmin >= 0 and ymin >= 0 and xmax <= img_w and ymax <= img_h:
-                                box = DetectBox(cl, conf, xmin, ymin, xmax, ymax)
-                                detectResult.append(box)
+                            xmin = xmin if xmin > 0 else 0
+                            ymin = ymin if ymin > 0 else 0
+                            xmax = xmax if xmax < img_w else img_w
+                            ymax = ymax if ymax < img_h else img_h
+
+                            box = DetectBox(cl, conf, xmin, ymin, xmax, ymax)
+                            detectResult.append(box)
 
     # NMS 过程
     print('detectResult:', len(detectResult))
