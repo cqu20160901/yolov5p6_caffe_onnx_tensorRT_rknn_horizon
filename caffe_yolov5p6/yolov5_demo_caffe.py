8a3136f2bef00c6c14cb73be8bb73d98b15db692
业余程序猿 <1013429609@qq.com>
2022-08-31 19:28:22 +0800

    Update yolov5_demo_caffe.py

--- caffe_yolov5p6/yolov5_demo_caffe.py
+++ caffe_yolov5p6/yolov5_demo_caffe.py
@@ -20,7 +20,7 @@ if not os.path.exists(net_file):
 
 net = caffe.Net(net_file, caffe_model, caffe.TEST)
 
-CLASSES = ('car', 'ped')
+CLASSES = ['car', 'ped']
 
 class_num = 2
 anchor_num = 3
@@ -56,8 +56,8 @@ def grid_cell_init():
     for index in range(output_head):
         for w in range(cell_size[index][1]):
             for h in range(cell_size[index][0]):
-                grid_cell[index][h][w][0] = w;
-                grid_cell[index][h][w][1] = h;
+                grid_cell[index][h][w][0] = w
+                grid_cell[index][h][w][1] = h
 
 
 def IOU(xmin1, ymin1, xmax1, ymax1, xmin2, ymin2, xmax2, ymax2):
@@ -169,6 +169,7 @@ def preprocess(src):
 
 def detect(imgfile):
     origimg = cv2.imread(imgfile)
+    origimg = cv2.cvtColor(origimg, cv2.COLOR_BGR2RGB)
     img_h, img_w = origimg.shape[:2]
     img = preprocess(origimg)
 
